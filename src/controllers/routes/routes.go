dfa67cc337f43c6be2822ff79055413d32f2af0d
dannielss <daniel_sousa.p@outlook.com>
2023-01-03 22:59:30 -0300

    feat: create gin routes

---
+++ src/controllers/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/dannielss/go-crud/src/controllers/userController"
+	"github.com/gin-gonic/gin"
+)
+func initRoutes(r *gin.RouterGroup) {
+	r.GET("/user/:userId", userController.ShowUser)
+	r.GET("/userByEmail/:userEmail", userController.ShowUserByEmail)
+	r.POST("/user", userController.CreateUser)
+	r.PUT("/user/:userId", userController.UpdateUser)
+	r.DELETE("/user/:userId", userController.DestroyUser)
+}
