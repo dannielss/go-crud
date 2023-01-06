938bbf00edbada74b6df0a0b80485f22dd6b5470
dannielss <daniel_sousa.p@outlook.com>
2023-01-05 23:31:29 -0300

    feat: showing error if data received there are some errors

--- src/controllers/userController/insert.go
+++ src/controllers/userController/insert.go
@@ -1,5 +1,23 @@
 package userController
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/dannielss/go-crud/src/configuration/rest_err"
+	"github.com/dannielss/go-crud/src/controllers/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := 
+			rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
