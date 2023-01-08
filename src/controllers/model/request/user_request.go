6b51ea4f1648c82c8fc4909201ebe7ed0b9eea77
dannielss <daniel_sousa.p@outlook.com>
2023-01-07 21:04:08 -0300

    feat: validating password, age and email

--- src/controllers/model/request/user_request.go
+++ src/controllers/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email string `json:"email" binding:""`
-	Password string `json:"password" binding:""`
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#$%&*"`
 	Name string `json:"name" binding:"required,min=4,max=100"`
-	Age int8 `json:"age" binding:""`
+	Age int8 `json:"age" binding:"required,min=1,max=140"`
 }
