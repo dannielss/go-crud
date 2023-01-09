c666704a0437ed4779fd68e2c75883515a9c37d5
dannielss <daniel_sousa.p@outlook.com>
2023-01-08 23:36:58 -0300

    feat: using new function ValidateUserError on insert function

--- src/controllers/userController/insert.go
+++ src/controllers/userController/insert.go
@@ -3,7 +3,7 @@ package userController
 import (
 	"fmt"
 
-	"github.com/dannielss/go-crud/src/configuration/rest_err"
+	"github.com/dannielss/go-crud/src/configuration/validation"
 	"github.com/dannielss/go-crud/src/controllers/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,8 +12,7 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := 
-			rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
