dd8c4d696de1b3fb4d133cc156bb70d08fe99706
dannielss <daniel_sousa.p@outlook.com>
2023-01-11 22:48:02 -0300

    feat: craete info and error func logs

--- src/configuration/logger/logger.go
+++ src/configuration/logger/logger.go
@@ -33,6 +33,17 @@ func init() {
 	log, _ = logConfig.Build()
 }
 
+func Info(message string, tags ...zap.Field) {
+	log.Info(message, tags...)
+	log.Sync()
+}
+
+func Error(message string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Info(message, tags...)
+	log.Sync()
+}
+
 func getOutputLogs() string {
 	output := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_OUTPUT)))
 
