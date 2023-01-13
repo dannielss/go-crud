112f60eb3b0c33ec28f4bf92716917998027af45
dannielss <daniel_sousa.p@outlook.com>
2023-01-12 23:46:04 -0300

    faet: add create user service

---
+++ src/model/create_user.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"github.com/dannielss/go-crud/src/configuration/logger"
+	"github.com/dannielss/go-crud/src/configuration/rest_err"
+	"go.uber.org/zap"
+)
+
+func (ud *UserDomain) CreateUser() *rest_err.RestErr {
+	logger.Info("Init createUser model", zap.String("journey", "createUser"))
+
+	ud.EncryptPassword()
+
+	return nil
+}
