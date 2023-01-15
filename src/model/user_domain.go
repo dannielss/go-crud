2076313ef34c9434e10bfae3abaa584d60bb0e32
dannielss <daniel_sousa.p@outlook.com>
2023-01-15 16:41:54 -0300

    feat: create getters

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -28,6 +28,22 @@ type userDomain struct {
 	age int8
 }
 
+func (ud *userDomain) getEmail() string {
+	return ud.email
+}
+
+func (ud *userDomain) GetPassword() string {
+	return ud.password
+}
+
+func (ud *userDomain) GetName() string {
+	return ud.name
+}
+
+func (ud *userDomain) GetAge() int8 {
+	return ud.age
+}
+
 func (ud *userDomain) EncryptPassword() {
 	hash := md5.New()
 	defer hash.Reset()
