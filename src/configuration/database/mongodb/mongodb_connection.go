3517174ddc1019f386504058b7612d0d08f77a8b
dannielss <daniel_sousa.p@outlook.com>
2023-01-19 23:50:31 -0300

    feat: create InitConnection func

---
+++ src/configuration/database/mongodb/mongodb_connection.go
@@ -0,0 +1,21 @@
+package mongodb
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitConnection() {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		panic(err)
+	}
+}
