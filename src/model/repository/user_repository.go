6478f8ce4bb44f2edea53080fb3570d2e5a4a7f8
dannielss <daniel_sousa.p@outlook.com>
2023-02-07 00:27:04 -0300

    feat: create initialize user repository func and user repository struct

--- src/model/repository/user_repository.go
+++ src/model/repository/user_repository.go
@@ -3,7 +3,20 @@ package repository
 import (
 	"github.com/dannielss/go-crud/src/configuration/rest_err"
 	"github.com/dannielss/go-crud/src/model"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func NewUserRepository(
+	database *mongo.Database,
+) UserRepository {
+	return &userRepository{
+		database,
+	}
+}
+
+type userRepository struct {
+	databaseConnection *mongo.Database
+}
 
 type UserRepository interface {
 	CreateUser(userDomain model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestErr)
